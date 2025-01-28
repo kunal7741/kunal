a3f6fca51f433af3cad1be7d7de6ad3c000a5dec
kunal7741 <jhakunal7741@gmail.com>
2025-01-28 21:36:13 +0530

    Update and rename speedy.js to K

--- speedy.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const navItems = document.querySelector('.nav-items');
-
-toggleBtn.addEventListener('click', () => {
-  navItems.classList.toggle('active');
-});
